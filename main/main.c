c725763c64da4a1c8509aa85edac05b51f9a1710
Dmitriy <dmtshirokov1@outlook.com>
2016-06-14 16:18:06 +0300

    main.c
    Added startup file for a simple project

---
+++ main/main.c
@@ -0,0 +1,13 @@
+//helloWorld.c
+
+#include <iostream>
+using namespace std;
+
+//"Hello World!\n" as a simple output
+void simpleOuter() { cout << "Hello World!"\n; }
+
+//startup project
+main () {
+  simpleOuter();
+  return 0;
+}
