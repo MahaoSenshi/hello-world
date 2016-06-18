5a253a44aa5774b2b47c7c2d3c8c3318ed83ff1b
Dmitriy <dmtshirokov1@outlook.com>
2016-06-19 02:21:05 +0300

    Pointers
    Modified a method "GetSimpleExample". 
    Was changed type for pointers.

--- main/pointers.c
+++ main/pointers.c
@@ -13,22 +13,24 @@ void GetPointerDescribe() {
     printf("The pointer 'pnt_var' is located at 0x%08x, contains 0x%08x and points to %d.\n", &pnt_var, pnt_var, *pnt_var);
 }
 
+//modified pointers
 void GetSimpleExample() {
     int int_arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     char char_arr[10] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
     
-    //create two pointers for this arrays
-    int *pnt_int_arr = int_arr;
-    char *pnt_char_arr = char_arr;
+    //create one pointer with type void
+    void *pnt_void;
     
     int i;
+    pnt_void = (void *) int_arr;
     for (i = 0; i < 10; i++) {
-        printf("int_arr[%d] contains %d and is located %p;\n", i, *pnt_int_arr, pnt_int_arr);
-        pnt_int_arr = pnt_int_arr + 1;
+        printf("int_arr[%d] contains %d and is located %p;\n", i, *((int *)pnt_void), pnt_void);
+        pnt_void = (void *)((int *)pnt_void + 1);
     }
     
+    pnt_void = (void *) char_arr;
     for (i = 0; i < 10; i++) {
-        printf("char_arr[%d] contains %c and is located %p;\n", i + 1, *pnt_char_arr, pnt_char_arr);
-        pnt_char_arr = pnt_char_arr + 1;
+        printf("char_arr[%d] contains %c and is located %p;\n", i, *((char *)pnt_void), pnt_void);
+        pnt_void = (void *)((char *)pnt_void + 1);
     }
 }
