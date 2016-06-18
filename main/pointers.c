f1aec8923aa8783dd51812112fa47acaa52b41c3
Dmitriy <dmtshirokov1@outlook.com>
2016-06-19 01:55:16 +0300

    Pointers
    Added method "GetSimpleExample()"

--- main/pointers.c
+++ main/pointers.c
@@ -12,3 +12,23 @@ void GetPointerDescribe() {
     printf("The variable 'var' is located at 0x%08x and contains %d.\n", &var, var);
     printf("The pointer 'pnt_var' is located at 0x%08x, contains 0x%08x and points to %d.\n", &pnt_var, pnt_var, *pnt_var);
 }
+
+void GetSimpleExample() {
+    int int_arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    char char_arr[10] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+    
+    //create two pointers for this arrays
+    int *pnt_int_arr = int_arr;
+    char *pnt_char_arr = char_arr;
+    
+    int i;
+    for (i = 0; i < 10; i++) {
+        printf("int_arr[%d] contains %d and is located %p;\n", i, *pnt_int_arr, pnt_int_arr);
+        pnt_int_arr = pnt_int_arr + 1;
+    }
+    
+    for (i = 0; i < 10; i++) {
+        printf("char_arr[%d] contains %c and is located %p;\n", i + 1, *pnt_char_arr, pnt_char_arr);
+        pnt_char_arr = pnt_char_arr + 1;
+    }
+}
