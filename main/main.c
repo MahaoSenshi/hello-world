c978daf211e3e9a6d5d426e2b7796435cc7ac853
Dmitriy <dmtshirokov1@outlook.com>
2016-07-12 03:05:34 +0300

    Update main.c
    Added the methods from pointer class

--- main/main.c
+++ main/main.c
@@ -1,10 +1,20 @@
 //helloWorld.c
 
 #include <iostream>
+#include <pointer.h>
+
 using namespace std;
 
 //"Hello World!\n" as a simple output
-void simpleOuter() { cout << "Hello World!"\n; }
+void simpleOuter() { 
+  cout << "-------Pointer overview-------"\n; 
+  cout << "-------Pointer Describe-------\n";
+  GetPointerDescribe();
+  cout << "-------Simple Example 1-------\n";
+  GetSimpleExample();
+  cout << "-------Simple Example 2-------\n";
+  GetSimpleExample2();
+}
 
 //startup project
 main () {
