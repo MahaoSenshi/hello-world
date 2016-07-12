96d55f600b094742a3e433c048fd6da1a38e1234
Dmitriy <dmtshirokov1@outlook.com>
2016-07-12 03:10:04 +0300

    Revert "Update main.c"
    This reverts commit c978daf211e3e9a6d5d426e2b7796435cc7ac853.

--- main/main.c
+++ main/main.c
@@ -1,20 +1,10 @@
 //helloWorld.c
 
 #include <iostream>
-#include <pointer.h>
-
 using namespace std;
 
 //"Hello World!\n" as a simple output
-void simpleOuter() { 
-  cout << "-------Pointer overview-------"\n; 
-  cout << "-------Pointer Describe-------\n";
-  GetPointerDescribe();
-  cout << "-------Simple Example 1-------\n";
-  GetSimpleExample();
-  cout << "-------Simple Example 2-------\n";
-  GetSimpleExample2();
-}
+void simpleOuter() { cout << "Hello World!"\n; }
 
 //startup project
 main () {
