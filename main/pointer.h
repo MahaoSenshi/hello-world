efffa0cfbafb1cee1fd381bb5cf2283218e2834c
Dmitriy <dmtshirokov1@outlook.com>
2016-07-29 01:26:13 +0300

    Update pointer.h
    Fixed the pnt_var point

--- main/pointer.h
+++ main/pointer.h
@@ -7,7 +7,7 @@ void GetPointerDescribe() {
     int *pnt_var;
     
     //get memory address for "var" variable
-    *pnt_var = &var;
+    pnt_var = &var;
     
     printf("The variable 'var' is located at 0x%08x and contains %d.\n", &var, var);
     printf("The pointer 'pnt_var' is located at 0x%08x, contains 0x%08x and points to %d.\n", &pnt_var, pnt_var, *pnt_var);
